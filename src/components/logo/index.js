0c3279cb622b45f7ba52c599f401f240e8a1a0bd
Your Name <you@example.com>
2022-09-26 18:31:32 +0100

    scroller completed

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -6,38 +6,116 @@ export const Logos = () => {
 		<div class='skills-scroller'>
 			<div class='skills-container'>
 				<div class='skills-card'>
-					<i class='devicon-html5-plain'></i>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+						alt='html-logo'
+					/>
+
 					<h3 class='skills-card-info'>HTML:5</h3>
 				</div>
 				<div class='skills-card'>
-					<i class='devicon-css3-plain'></i>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+						alt='css logo'
+					/>
 					<h3 class='skills-card-info'>CSS</h3>
 				</div>
 				<div class='skills-card'>
-					<i class='devicon-javascript-plain'></i>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+						alt='javascript-logo'
+					/>
+
 					<h3 class='skills-card-info'>JAVASCRIPT</h3>
 				</div>
 				<div class='skills-card'>
-					<i class='devicon-react-original'></i>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+						alt='react-logo'
+					/>
+
 					<h3 class='skills-card-info'>REACT</h3>
+				</div>
+				<div class='skills-card'>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg'
+						alt='mongodb-logo'
+					/>
+
+					<h3 class='skills-card-info'>MONGODB</h3>
+				</div>
+				<div class='skills-card'>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
+						alt='mysql-logo'
+					/>
+
+					<h3 class='skills-card-info'>MYSQL</h3>
+				</div>
+				<div class='skills-card'>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg'
+						alt='graph-ql-logo'
+					/>
+
+					<h3 class='skills-card-info'>GRAPHQL</h3>
 				</div>
 			</div>
 			<div class='skills-container'>
 				<div class='skills-card'>
-					<i class='devicon-html5-plain'></i>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+						alt='html-logo'
+					/>
+
 					<h3 class='skills-card-info'>HTML:5</h3>
 				</div>
 				<div class='skills-card'>
-					<i class='devicon-css3-plain'></i>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+						alt='css logo'
+					/>
 					<h3 class='skills-card-info'>CSS</h3>
 				</div>
 				<div class='skills-card'>
-					<i class='devicon-javascript-plain'></i>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+						alt='javascript-logo'
+					/>
+
 					<h3 class='skills-card-info'>JAVASCRIPT</h3>
 				</div>
 				<div class='skills-card'>
-					<i class='devicon-react-original'></i>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+						alt='react-logo'
+					/>
+
 					<h3 class='skills-card-info'>REACT</h3>
+				</div>
+				<div class='skills-card'>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg'
+						alt='mongodb-logo'
+					/>
+
+					<h3 class='skills-card-info'>MONGODB</h3>
+				</div>
+				<div class='skills-card'>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
+						alt='mysql-logo'
+					/>
+
+					<h3 class='skills-card-info'>MYSQL</h3>
+				</div>
+				<div class='skills-card'>
+					<img
+						src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg'
+						alt='graph-ql-logo'
+					/>
+
+					<h3 class='skills-card-info'>GRAPHQL</h3>
 				</div>
 			</div>
 		</div>
